f4a6ec05b311b3ced9568a33f94878a6e16bf221
kaewkloaw <punchaya.chan@gmail.com>
2025-11-18 11:19:30 +0700

    update components and App.jsx

--- Main/src/client/App.jsx
+++ Main/src/client/App.jsx
@@ -1,11 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MicVideo from "./components/Home.jsx";
+import MicVideo from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import * as LoginModule from "./components/Login";
+import * as SignUpModule from "./components/SignUp";
+
+// Resolve either default export or named export (defensive for mixed module styles)
+const Login = LoginModule.default ?? LoginModule.Login ?? (() => null);
+const SignUp = SignUpModule.default ?? SignUpModule.SignUp ?? (() => null);
+import { useState } from "react";
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <BrowserRouter>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
         <Route path="/" element={<MicVideo />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signup" element={<SignUp setIsLoggedIn={setIsLoggedIn} />} />
       </Routes>
     </BrowserRouter>
   );
